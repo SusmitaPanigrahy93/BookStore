cc5fbad568ce9805aac94469e95bf655b75ea83a
Susmita93 <susmitapanigrahy93@gmail.com>
2025-04-08 18:42:45 +0530

    Commit for Books.jsx and bookSlice.jsx files

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -1,54 +1,81 @@
-import { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { addBook, removeBook } from '../redux/books/bookSlice';
+import { addBooks, fetchBooks, removeBooks } from '../redux/books/bookSlice';
 import { v4 as uuidv4 } from 'uuid';
 
 const Books = () => {
     const dispatch = useDispatch();
     const books = useSelector(state => state.books.list);
+    console.log('Books list from Redux:', books);
 
     const[title, setTitle] = useState('');
     const[author, setAuthor] = useState('');
+    const[searchValue, setSearchValue] = useState('');
+
+    useEffect(() => {
+        dispatch(fetchBooks());
+    },[dispatch]);
 
     const handleAddBook = () => {
         if (!title || !author)
             return;
-        dispatch(addBook({ id: uuidv4(), title, author }));
+        const newBook = { id: uuidv4(), title, author };
+        dispatch(addBooks(newBook));
+        console.log('Book Added ', newBook);
         setTitle('');
         setAuthor('');
     };
     const handleRemoveBook = (id) => {
-        dispatch(removeBook(id));
+        dispatch(removeBooks(id));
     };
 
     return (
-        <div style={{ padding: '20px' }}>
+        <div className="container">
             <h2>📚 Book List</h2>
+            <input 
+                className="search-input"
+                type="text"
+                placeholder="search by title or author"
+                value={searchValue}
+                onChange={e => setSearchValue(e.target.value)}
+            /> 
             <ul>
-                {books.map(book => {
-                    <li key = {book.id}>
+                {books
+                    .filter(book => 
+                        book.title.toLowerCase().includes(searchValue.toLowerCase()) ||
+                        book.author.toLowerCase().includes(searchValue.toLocaleLowerCase())
+                    )
+                    .map(book => {
+                    return(
+                    <div className='book-item' key = {book.id}>
                         <strong>{book.title}</strong> by {book.author}
-                        <button onClick={() => handleRemoveBook(book.id)} style={{marginLeft: '10px'}}></button>
-                    </li>
+                        <button onClick={() => handleRemoveBook(book.id)} style={{marginLeft: '10px'}}>Remove Book</button>
+                    </div>
+                );
                 })}
             </ul>
-
-            <h3>Add a Book</h3>
+            <div className="form-container">
+            <div className="input-row">   
+            <h3 className="add-book-title">Add a Book</h3>
             <input 
                 type="text"
                 placeholder="title"
-                value="title"
+                value={title}
                 onChange={e => setTitle(e.target.value)}
                 style={{ marginRight: '10px' }}
             /> 
             <input 
                 type="text"
                 placeholder="author"
-                value="author"
+                value={author}
                 onChange={e => setAuthor(e.target.value)}
                 style={{ marginRight: '10px' }}
             /> 
+            </div>
+            <div className="button-wrapper">
             <button onClick={handleAddBook}>Add Book</button>
+            </div>
+            </div>
         </div>
     );
 };
--- src/redux/books/bookSlice.js
+++ src/redux/books/bookSlice.js
@@ -1,4 +1,5 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { fetchBooksFromAPI } from "../../api/fakeBooksAPI";
 
 const initialState = {
     list: [],
@@ -6,19 +7,46 @@ const initialState = {
     error: null,
 };
 
+export const fetchBooks = createAsyncThunk(
+    'books/fetchBooks',
+    async(__, thunkAPI) => {
+        try {
+            const response = await fetchBooksFromAPI();
+            return response;
+        } catch(error) {
+            return thunkAPI.rejectWithValue(error.message);
+        }
+    }
+);
+
 const bookSlice = createSlice({
-    name: Books,
+    name: 'books',
     initialState,
     reducers: {
         setBooks(state, action) {
             state.list = action.payload;
         },
         addBooks(state, action) {
-            state.list.push(action,payload);
+            state.list.push(action.payload);
         },
-        removeBooks(state,payload) {
+        removeBooks(state,action) {
             state.list = state.list.filter(book => book.id !== action.payload);
         },
+    },
+    extraReducers: builder => {
+        builder
+            .addCase(fetchBooks.pending, state => {
+                state.loading = true;
+                state.error = null;
+            })
+            .addCase(fetchBooks.fulfilled, (state, action) => {
+                state.loading = false;
+                state.list = action.payload;
+            })
+            .addCase(fetchBooks.rejected, (state, action) => {
+                state.loading = false;
+                state.error = action.payload;
+            });
     },
 });
 
